7ce4e3ad7ad66d72e4ab3510cdb6971a6033c45a
Picrik's Lab <picrik@live.fr>
2019-05-05 23:27:55 +0200

    Update and rename solution.py to solutions.py

--- solutions.py
+++ solutions.py
@@ -79,3 +79,12 @@ def sumNonAdj(liste):
             if val < (liste[i] + liste[j]):
                 val = liste[i] + liste[j]
     return val
+
+#6
+#Given a singly linked list and an integer k, remove the kth last element from the list.
+#k is guaranteed to be smaller than the length of the list.
+#The list is very long, so making more than one pass is prohibitively expensive.
+#Do this in constant space and in one pass.
+
+def returnFirst(Liste, k):
+    return Liste[:-k]
